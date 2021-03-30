dc57d78e3246cc5b9c4f40341539156a87622a6f
shrekt444 <andrea.maie444@gmail.com>
2021-03-30 15:40:05 +0200

    check funzionano

--- js/script.js
+++ js/script.js
@@ -1,52 +1,19 @@
 //bottone calculate
-var genButton = document.getElementById('gen-button');
-genButton.addEventListener('click', function() {
+var calcButton = document.getElementById('btn-calc');
+calcButton.addEventListener('click', function() {
 
-    //operazioni al click
-
-    var userName = document.getElementById('generator-nome-cognome').value;
-    console.log(userName);
-
-    var kmPercorrere = document.getElementById('generator-km').value;
-    kmPercorrere = parseFloat(kmPercorrere);
-    console.log(kmPercorrere);
-
-    //offerta
-    var genUserAge = document.getElementById('generator-eta');
-    var userAge = genUserAge.value;
-    console.log(userAge);
-    var userOffer = 'Prezzo Standard';
-
-    // prezzo biglietto
-    var prezzoBiglietto = kmPercorrere * 0.21; 
-    console.log(prezzoBiglietto);
-
-    var percent;
-    console.log(percent);
-
-    if ( userAge == 'minorenne' ) {
-        percent = ( prezzoBiglietto *  20 ) / 100;
-        prezzoBiglietto = prezzoBiglietto - percent;
-        userOffer = 'Sconto Minorenne';
-    } else if ( userAge == 'over' ) {
-        percent = ( prezzoBiglietto *  40 ) / 100;
-        prezzoBiglietto = prezzoBiglietto - percent;
-        userOffer = 'Sconto Over';
+    var prezzoBaseBurger = 50;
+    
+    var addonCheck = document.getElementsByClassName('addon');
+    console.log(addonCheck);
+    for( var i = 0; i < addonCheck.length; i++) {
+        var thisCheck = addonCheck[i];
+        console.log( i, thisCheck.checked);
+        console.log( i, thisCheck.value);
     }
 
-    //carrozza
-    var numeroCarrozza = Math.floor(Math.random() * 15) +1;
-    console.log(numeroCarrozza);
-
-    //codice
-    var numeroCodice = Math.floor(Math.random() * 10000) +1;
-    console.log(numeroCodice);
 
     //stampare
-    document.getElementById('result-user').innerHTML = userName;
-    document.getElementById('result-offer').innerHTML = userOffer;
-    document.getElementById('result-carrozza').innerHTML = numeroCarrozza;
-    document.getElementById('result-codice').innerHTML = numeroCodice;
-    document.getElementById('prezzo').innerHTML = prezzoBiglietto.toFixed(2);
+    //document.getElementById('price').innerHTML = prezzoBiglietto.toFixed(2);
 
 });
